bb604c473c7d41f0ef363bb30264038c0a3c8ce4
Apoorva Singh <apoorvasingh157@gmail.com>
2020-10-21 17:04:04 +0530

    Switched to bootstrap 4

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,10 +2,12 @@ import Calendar from 'tui-calendar';
 import moment from 'moment';
 import Chance from 'chance';
 
+
 // All required CSS files
 import "tui-calendar/dist/tui-calendar.css";
 import 'tui-date-picker/dist/tui-date-picker.css';
 import 'tui-time-picker/dist/tui-time-picker.css';
+
 
 // To generate random IDs for schedules
 let chance = new Chance();
--- webpack.common.js
+++ webpack.common.js
@@ -11,7 +11,7 @@ module.exports = {
   mode: 'development',
   entry: {
     main: './src/index.js',
-    vendor: './src/vendor.js'
+    // vendor: './src/vendor.js'
   },
   module: {
     rules: [
